e6c43baf0b4962569c5affa6058886a47b86dfe1
Hugo Ramos Bello <hugorb45@gmail.com>
2021-09-20 07:40:57 -0300

    Criação da entidade Post

---
+++ src/main/java/com/sharkpay/myspace/entities/Post.java
@@ -0,0 +1,95 @@
+package com.sharkpay.myspace.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private LocalDate moment;
+	private String title;
+	private String content;
+	private Integer likes;
+	private User author;
+	
+	public Post() {
+	}
+	
+	public Post(LocalDate moment, String title, String content, Integer likes, User author) {
+		id = null;
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+		this.author = author;
+	}
+
+	public LocalDate getMoment() {
+		return moment;
+	}
+
+	public void setMoment(LocalDate moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+}
